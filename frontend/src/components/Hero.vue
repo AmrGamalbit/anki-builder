eaeacfec3b8437ea56ae7a40ac5d803a8ab96bcc
Amr Gamal <amrgamal.agm@gmail.com>
2026-02-13 14:25:29 +0200

    feat: add custom font and update hero styles

--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="bg-primary p-16">
+  <section class="bg-primary p-16 font-koho">
     <h2 class="text-surface text-5xl font-semibold italic">AnkiBuilder</h2>
+    <h4 class="text-surface font-semibold italic mb-10">Create smart Anki cards in seconds</h4>
     <div class="grid grid-cols-2 my-4 gap-10">
-      <div>
-        <div class="bg-primary-muted text-surface p-5 rounded-2xl flex-center h-38">What does AnkiBuilder Include?</div>
-        <div class="bg-accent rounded-2xl flex-center h-38 p-5">
+      <div class="grid gap-5 text-2xl">
+        <div class="bg-primary-muted text-surface p-5 rounded-2xl flex-center">
+          What does AnkiBuilder Include?
+        </div>
+        <div class="bg-accent rounded-2xl flex-center p-5 text-neutral">
           Free and premium flashcard decks with contextual sentences, translations, and natural
           pronunciation. 25+ languages available.
         </div>
       </div>
-      <div class="bg-surface rounded-2xl flex-center h-76">Ich Bin Amr</div>
+      <div class="bg-surface rounded-2xl flex-center text-4xl font-medium text-neutral">
+        Ich Bin Amr
+      </div>
     </div>
   </section>
 </template>
