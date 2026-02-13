b78d6b06194c11ecc1b04a2dcf014586ee5b91dd
Amr Gamal <amrgamal.agm@gmail.com>
2026-02-13 13:42:51 +0200

    feat: add hero section to home view

---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="bg-primary p-16">
+    <h2 class="text-surface text-5xl font-semibold italic">AnkiBuilder</h2>
+    <div class="grid grid-cols-2 my-4 gap-10">
+      <div>
+        <div class="bg-primary-muted text-surface p-5 rounded-2xl flex-center h-38">What does AnkiBuilder Include?</div>
+        <div class="bg-accent rounded-2xl flex-center h-38 p-5">
+          Free and premium flashcard decks with contextual sentences, translations, and natural
+          pronunciation. 25+ languages available.
+        </div>
+      </div>
+      <div class="bg-surface rounded-2xl flex-center h-76">Ich Bin Amr</div>
+    </div>
+  </section>
+</template>
