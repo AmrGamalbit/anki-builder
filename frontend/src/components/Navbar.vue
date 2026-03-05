866a598a43a5d8d88c933bc5860adf04db4915fb
Amr Gamal <amrgamal.agm@gmail.com>
2026-03-05 02:06:09 +0200

    feat: integrate icons into navbar and fix alignment

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MagicIcon from './navbar-icons/MagicIcon.vue';
+import SliderIcon from './navbar-icons/SlidersIcon.vue';
+import QuestionIcon from './navbar-icons/QuestionIcon.vue';
+</script>
 
 <template>
-  <nav class="bg-neutral flex justify-between align-center text-surface p-2 font-bold">
+  <nav class="bg-neutral flex justify-between items-baseline text-surface p-2 font-bold">
     <RouterLink class="cursor-pointer text-2xl" to="/">AnkiBuilder</RouterLink>
-    <ul class="flex list-none gap-2">
-      <li><button class="cursor-pointer">Export</button></li>
-      <li><button class="cursor-pointer">Settings</button></li>
+    <ul class="flex list-none gap-5">
+      <li>
+        <RouterLink to="/generator" class="cursor-pointer">
+          <MagicIcon />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/settings" class="cursor-pointer">
+          <SliderIcon />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/about" class="cursor-pointer">
+          <QuestionIcon />
+        </RouterLink>
+      </li>
     </ul>
   </nav>
 </template>
