3ca86205b866d675454454c80b3d6ea547e069a7
Amr Gamal <amrgamal.agm@gmail.com>
2026-07-02 22:46:39 +0300

    feat: use FilePicker in FileInput component

--- frontend/src/components/generator/steps/words/FileInput.vue
+++ frontend/src/components/generator/steps/words/FileInput.vue
@@ -3,9 +3,10 @@ import '@/assets/global.css';
 import OptionRow from '@/components/ui/OptionField.vue';
 import type { SchemaField } from '@/types/schema';
 import { useGeneratorStore } from '@/stores/generator';
+import FilePicker from '@/components/ui/FilePicker.vue';
 
 const generatorStore = useGeneratorStore();
-const content = defineModel('content');
+const content = defineModel<File | null>('content');
 const fileOptionsSchema: Record<string, SchemaField> = {
   delimiter: {
     label: 'Delimiter',
@@ -26,41 +27,8 @@ const fileOptions = generatorStore.contentOptions.file;
 </script>
 
 <template>
-  <section class="flex flex-col gap-5">
-    <div class="w-full py-9 bg-white rounded-sm border border-dashed border-gray-300">
-      <div class="flex flex-col items-center gap-4">
-        <div class="flex flex-col items-center gap-1">
-          <svg
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          ></svg>
-          <p class="text-gray-400 text-xs">CSV, XLC or XLCX, smaller than 15MB</p>
-        </div>
-
-        <div class="flex flex-col items-center gap-2">
-          <p class="text-gray-900 text-sm font-medium">Drag and Drop your file here or</p>
-          <label class="cursor-pointer">
-            <input
-              type="file"
-              class="hidden"
-              @change="
-                (e) => {
-                  content = (e.target as HTMLInputElement).files?.[0];
-                }
-              "
-            />
-            <span
-              class="flex w-28 h-9 px-2 bg-primary-muted rounded-full shadow text-white text-xs font-semibold items-center justify-center"
-            >
-              Choose File
-            </span>
-          </label>
-        </div>
-      </div>
-    </div>
+  <section>
+    <FilePicker :accept="'image/*'" v-model="content" />
     <div class="flex flex-col gap-4 p-3">
       <OptionRow
         v-for="(option, key) in fileOptionsSchema"
